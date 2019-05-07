5cfb7a83f8881f85c20aaa47af5e93863c905c51
reboot0501 <48519882+reboot0501@users.noreply.github.com>
2019-05-07 23:13:00 +0900

    NavLink & Component 내부에서 URL 이동 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+/**
+ * 1차 Route & Header 메뉴 (Link) 적용
+ * 2차 Component 내부에서 Link 적용 Posts Component 반영
+ */
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './routes/Home';
 import About from './routes/About';
+import Posts from './routes/Posts';
 
 function App() {
   return (
@@ -13,6 +18,7 @@ function App() {
         <div> { /* 실제 URL 이동처리 */ }
           <Route exact path="/" component={Home} />
           <Route path="/about/:username" component={About} /> { /*Route URL Paramter 전달 -> /:username */ }
+          <Route path="/posts" component={Posts} />
         </div>
       </div>
     </Router>
