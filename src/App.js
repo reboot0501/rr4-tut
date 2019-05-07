36470af09029efd04d4de25dc93d5598a94aac66
reboot0501 <48519882+reboot0501@users.noreply.github.com>
2019-05-07 23:39:29 +0900

    URL Query String 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 /**
  * 1차 Route & Header 메뉴 (Link) 적용
  * 2차 NavLink (CSS 적용) 이동적용, Component 내부에서 Link 적용 Posts Component 반영
+ * 3차 Redirect 적용, MyPage / Login Component 적용
+ * 4차 URL Query String 적용
  */
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -9,6 +11,9 @@ import Header from './components/Header';
 import Home from './routes/Home';
 import About from './routes/About';
 import Posts from './routes/Posts';
+import MyPage from './routes/MyPage';
+import Login from './routes/Login';
+import Search from './routes/Search';
 
 function App() {
   return (
@@ -19,6 +24,9 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/about/:username" component={About} /> { /*Route URL Paramter 전달 -> /:username */ }
           <Route path="/posts" component={Posts} />
+          <Route path="/login" component={Login}/>
+          <Route path="/me" component={MyPage}/>
+          <Route path="/search" component={Search}/>                  
         </div>
       </div>
     </Router>
